84d2d852dc4ffcec99d4c55a5c55059c93b7e028
louiscklaw <louiscklaw@gmail.com>
2020-08-09 21:46:33 +0800

    update,

---
+++ youtube-live-viewer-count/monitor.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -ex
+
+while true; do
+
+  ./run.sh
+
+  cd 3c71f08e4092e2057bdf583f7ec1ab56
+
+    cp ../viewer_count.json ./ZreBbmKtOoE_viewer_count.json
+    git add .
+    git commit . -m"update on $(date),"
+    git push
+
+  cd -
+
+
+  sleep 15
+
+done
