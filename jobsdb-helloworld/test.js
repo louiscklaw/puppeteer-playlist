fea1fabea28325292fe14e37c1315563d28d4f4a
louiscklaw <louiscklaw@gmail.com>
2020-08-21 02:25:48 +0800

    update and test,

--- jobsdb-helloworld/test.js
+++ jobsdb-helloworld/test.js
@@ -3,32 +3,15 @@ const puppeteer = require( 'puppeteer' );
 ( async () => {
   const browser = await puppeteer.launch({
     defaultViewport:{
-        width: 1920,
-        height: 2080
+        width: 1600,
+        height: 5080
     },
     ignoreHTTPSErrors: true
   });
   const page = await browser.newPage();
-  await page.goto( 'https://example.com' );
+  await page.goto( 'https://hk.jobsdb.com/hk/jobs/information-technology/1?Key=qa' );
   await page.screenshot( {
-    path: 'example.png'
-  } );
-
-  await browser.close();
-} )();
-
-( async () => {
-  const browser = await puppeteer.launch({
-    defaultViewport:{
-        width: 1920,
-        height: 2080
-    },
-    ignoreHTTPSErrors: true
-  });
-  const page = await browser.newPage();
-  await page.goto( 'https://facebook.com' );
-  await page.screenshot( {
-    path: 'facebook.png'
+    path: 'jobsdb_qa.png'
   } );
 
   await browser.close();
