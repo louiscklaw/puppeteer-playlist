2a899fe1722792551ffe411461fd6006195e4941
louiscklaw <louiscklaw@gmail.com>
2020-08-24 16:11:15 +0800

    update to add pending-xhr-puppeteer tryout,

--- pending-xhr-puppeteer-tryout/test.js
+++ pending-xhr-puppeteer-tryout/test.js
@@ -1,35 +1,21 @@
+#!/usr/bin/env node
+
 const puppeteer = require( 'puppeteer' );
+const { PendingXHR } = require('pending-xhr-puppeteer');
 
 ( async () => {
   const browser = await puppeteer.launch({
-    defaultViewport:{
-        width: 1920,
-        height: 2080
-    },
-    ignoreHTTPSErrors: true
+    headless: true,
   });
-  const page = await browser.newPage();
-  await page.goto( 'https://example.com' );
-  await page.screenshot( {
-    path: 'example.png'
-  } );
 
-  await browser.close();
-} )();
-
-( async () => {
-  const browser = await puppeteer.launch({
-    defaultViewport:{
-        width: 1920,
-        height: 2080
-    },
-    ignoreHTTPSErrors: true
-  });
   const page = await browser.newPage();
-  await page.goto( 'https://facebook.com' );
-  await page.screenshot( {
-    path: 'facebook.png'
-  } );
+  const pendingXHR = new PendingXHR(page);
+
+  await page.goto(`http://www.example.com`);
+  // // Here all xhr requests are not finished
+
+  await pendingXHR.waitForAllXhrFinished();
+  // // Here all xhr requests are finished
 
   await browser.close();
 } )();
