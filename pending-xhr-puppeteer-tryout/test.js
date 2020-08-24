0d48e860a70b1eeacd8d00f34f751dd6e9df4ae1
louiscklaw <louiscklaw@gmail.com>
2020-08-24 15:45:46 +0800

    update shortcut scripts,

---
+++ pending-xhr-puppeteer-tryout/test.js
@@ -0,0 +1,35 @@
+const puppeteer = require( 'puppeteer' );
+
+( async () => {
+  const browser = await puppeteer.launch({
+    defaultViewport:{
+        width: 1920,
+        height: 2080
+    },
+    ignoreHTTPSErrors: true
+  });
+  const page = await browser.newPage();
+  await page.goto( 'https://example.com' );
+  await page.screenshot( {
+    path: 'example.png'
+  } );
+
+  await browser.close();
+} )();
+
+( async () => {
+  const browser = await puppeteer.launch({
+    defaultViewport:{
+        width: 1920,
+        height: 2080
+    },
+    ignoreHTTPSErrors: true
+  });
+  const page = await browser.newPage();
+  await page.goto( 'https://facebook.com' );
+  await page.screenshot( {
+    path: 'facebook.png'
+  } );
+
+  await browser.close();
+} )();
