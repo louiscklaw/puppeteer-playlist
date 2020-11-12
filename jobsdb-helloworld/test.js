8177b3c7a2101c8bb5f5984f31fbefb61cc5261a
louiscklaw <louiscklaw@gmail.com>
2020-11-12 12:03:34 +0800

    init commit and test,

--- jobsdb-helloworld/test.js
+++ jobsdb-helloworld/test.js
@@ -3,12 +3,13 @@ const puppeteer = require( 'puppeteer' );
 ( async () => {
   const browser = await puppeteer.launch({
     defaultViewport:{
-        width: 1600,
+        width: 1920,
         height: 5080
     },
     ignoreHTTPSErrors: true
   });
   const page = await browser.newPage();
+
   await page.goto( 'https://hk.jobsdb.com/hk/jobs/information-technology/1?Key=qa' );
   await page.screenshot( {
     path: 'jobsdb_qa.png'
