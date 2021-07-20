c15124b777ef8b4e6fabcafffa8fcc6f6b0a8bba
louiscklaw <louiscklaw@gmail.com>
2021-07-20 22:36:16 +0800

    update test,

--- puppeteer-mocha-automated-testing/test/bootstrap.js
+++ puppeteer-mocha-automated-testing/test/bootstrap.js
@@ -1,30 +1,33 @@
-const puppeteer = require('puppeteer');
-const { expect } = require('chai');
-const opn = require('opn');
-const cmd = require('node-cmd');
+const puppeteer = require("puppeteer");
+const { expect } = require("chai");
+const opn = require("opn");
+const cmd = require("node-cmd");
 
-const _ = require('lodash');
+const _ = require("lodash");
 
-const globalVariables = _.pick(global, ['browser', 'expect']);
+const globalVariables = _.pick(global, ["browser", "expect"]);
 
 const opts = {
-    headless: false,
-    slowMo: 100,
-    timeout: 0,
-    args: ['--start-maximized', '--window-size=1366,786'] 
-}
+  headless: false,
+  slowMo: 10,
+  timeout: 0,
+  args: ["--start-maximized", "--window-size=1366,786"],
+};
 
-before (async () => {
+before(async () => {
   global.expect = expect;
   global.browser = await puppeteer.launch(opts);
 });
 
-
-after ( () => {
+after(() => {
   browser.close();
-  setTimeout( () => { cmd.run('node server.js');}, 5000)
-  setTimeout(() => {opn('http://localhost:9988');}, 2000);
- // open('./mochawesome-report/mochawesome.html');
+  setTimeout(() => {
+    cmd.run("node server.js");
+  }, 5000);
+  setTimeout(() => {
+    opn("http://localhost:9988");
+  }, 2000);
+  // open('./mochawesome-report/mochawesome.html');
   global.browser = globalVariables.browser;
   global.expect = globalVariables.expect;
 });
