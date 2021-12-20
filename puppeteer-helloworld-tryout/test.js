3ae90fc7e7564afef017d638955941ad2c0141cc
louiscklaw <louiscklaw@gmail.com>
2021-12-20 22:09:52 +0800

    update logging,

--- puppeteer-helloworld-tryout/test.js
+++ puppeteer-helloworld-tryout/test.js
@@ -1,5 +1,27 @@
-const puppeteer = require("puppeteer");
+// const puppeteer = require('puppeteer');
+// const chalk = require('chalk');
+import puppeteer from 'puppeteer';
+import chalk from 'chalk';
 
+const enablePageConsoleLog = (page) => {
+  page
+    .on('console', (message) => {
+      const type = message.type().substr(0, 3).toUpperCase();
+      const colors = {
+        LOG: (text) => text,
+        ERR: chalk.red,
+        WAR: chalk.yellow,
+        INF: chalk.cyan,
+      };
+      const color = colors[type] || chalk.blue;
+      console.log(color(`${type} ${message.text()}`));
+    })
+    .on('pageerror', ({ message }) => console.log(chalk.red(message)))
+    .on('response', (response) => console.log(chalk.green(`${response.status()} ${response.url()}`)))
+    .on('requestfailed', (request) => console.log(chalk.magenta(`${request.failure().errorText} ${request.url()}`)));
+};
+
+// start
 (async () => {
   const browser = await puppeteer.launch({
     defaultViewport: {
@@ -8,10 +30,13 @@ const puppeteer = require("puppeteer");
     },
     ignoreHTTPSErrors: true,
   });
+
   const page = await browser.newPage();
-  await page.goto("https://example.com");
+  enablePageConsoleLog(page);
+
+  await page.goto('https://example.com');
   await page.screenshot({
-    path: "example.png",
+    path: 'example.png',
   });
 
   await browser.close();
@@ -26,9 +51,9 @@ const puppeteer = require("puppeteer");
     ignoreHTTPSErrors: true,
   });
   const page = await browser.newPage();
-  await page.goto("https://facebook.com");
+  await page.goto('https://facebook.com');
   await page.screenshot({
-    path: "facebook.png",
+    path: 'facebook.png',
   });
 
   await browser.close();
