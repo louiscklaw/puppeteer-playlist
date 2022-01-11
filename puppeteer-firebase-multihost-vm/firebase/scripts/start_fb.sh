6fe44fc113b372f6901111975b86631792b74522
louiscklaw <louiscklaw@gmail.com>
2022-01-11 10:58:07 +0800

    update,

--- puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
+++ puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
@@ -14,4 +14,15 @@ pushd functions
   npm install
 popd
 
-nice firebase emulators:start
+nice firebase emulators:start &
+
+./scripts/wait-for-it.sh -t 120 localhost:9199
+./scripts/wait-for-it.sh -t 120 localhost:9099
+./scripts/wait-for-it.sh -t 120 localhost:9000
+./scripts/wait-for-it.sh -t 120 localhost:8080
+./scripts/wait-for-it.sh -t 120 localhost:5001
+./scripts/wait-for-it.sh -t 120 localhost:4000
+
+sleep 10
+
+echo 'firebase setup done'
