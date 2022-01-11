cd2d452b69cede3efcba6d6f4e4d4f7dee6f7140
louiscklaw <louiscklaw@gmail.com>
2022-01-11 11:01:09 +0800

    update,

--- puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
+++ puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
@@ -16,12 +16,12 @@ popd
 
 nice firebase emulators:start &
 
-./wait-for-it.sh -t 120 localhost:9199
-./wait-for-it.sh -t 120 localhost:9099
-./wait-for-it.sh -t 120 localhost:9000
-./wait-for-it.sh -t 120 localhost:8080
-./wait-for-it.sh -t 120 localhost:5001
-./wait-for-it.sh -t 120 localhost:4000
+./scripts/wait-for-it.sh -t 120 localhost:9199
+./scripts/wait-for-it.sh -t 120 localhost:9099
+./scripts/wait-for-it.sh -t 120 localhost:9000
+./scripts/wait-for-it.sh -t 120 localhost:8080
+./scripts/wait-for-it.sh -t 120 localhost:5001
+./scripts/wait-for-it.sh -t 120 localhost:4000
 
 sleep 10
 
