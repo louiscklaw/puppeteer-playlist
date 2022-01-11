9750650c8d8f92ffeceb5e2cc44311161bd6a776
louiscklaw <louiscklaw@gmail.com>
2022-01-11 22:46:57 +0800

    update,

--- scripts/jenkins_build.sh
+++ scripts/jenkins_build.sh
@@ -25,18 +25,18 @@ pushd pyppeteer-tryout
   bash test.sh
 popd
 
-# pushd pyppeteer-page-object-tryout
-#   bash test.sh
-# popd
-
-# pushd puppeteer-mocha-automated-testing
-#   bash scripts/build.sh
-# popd
-
 pushd jobsdb-helloworld
   bash build.sh
 popd
 
 # pushd youtube-live-viewer-count
 #   bash build.sh
+# popd
+
+# pushd pyppeteer-page-object-tryout
+#   bash test.sh
+# popd
+
+# pushd puppeteer-mocha-automated-testing
+#   bash scripts/build.sh
 # popd
