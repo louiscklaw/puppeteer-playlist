a460c712a56dd9d0f91530ace3edf79986f24716
louiscklaw <louiscklaw@gmail.com>
2022-01-11 21:20:40 +0800

    update,

--- puppeteer-firebase-multihost-vm/react_host_admin/App/src/App.js
+++ puppeteer-firebase-multihost-vm/react_host_admin/App/src/App.js
@@ -4,9 +4,6 @@ import UserFunctionalComponent from './components/UserFunctionalComponent';
 
 import './App.css';
 
-// import firestore from './firestore';
-// const db = firestore.firestore();
-
 function App() {
   return (
     <div className="App">
