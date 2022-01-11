defa0071d47079064b52cb5112268c053e656bd1
louiscklaw <louiscklaw@gmail.com>
2022-01-11 22:31:19 +0800

    update,

--- puppeteer-firebase-multihost-vm/scripts/test/clear.sh
+++ puppeteer-firebase-multihost-vm/scripts/test/clear.sh
@@ -4,15 +4,15 @@ set -ex
 
 export BROWSER=none
 
-pushd react_host_admin
+pushd react_host_admin/App
   scripts/clear.sh
 popd
   
-pushd react_host_client
+pushd react_host_client/App
   scripts/clear.sh
 popd
   
-pushd react_host_cms
+pushd react_host_cms/App
   scripts/clear.sh
 popd
   
