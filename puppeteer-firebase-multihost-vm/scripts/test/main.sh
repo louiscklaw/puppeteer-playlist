a4e49959dc16d7ee2a7b95b56332af0696201d96
louiscklaw <louiscklaw@gmail.com>
2022-01-11 11:06:44 +0800

    update,

--- puppeteer-firebase-multihost-vm/scripts/test/main.sh
+++ puppeteer-firebase-multihost-vm/scripts/test/main.sh
@@ -6,17 +6,17 @@ pushd firebase
   scripts/start_fb.sh
 popd
 
-# scripts/test/setup.sh
+scripts/test/setup.sh
 
-# pushd tests_host
-#   yarn --dev
-# popd
+pushd tests_host
+  yarn --dev
+popd
 
-# pushd tests_host
-#   yarn run int_test
-# popd
+pushd tests_host
+  yarn run int_test
+popd
 
-# echo 'test done'
+echo 'test done'
 
-# scripts/test/clear.sh
+scripts/test/clear.sh
 
