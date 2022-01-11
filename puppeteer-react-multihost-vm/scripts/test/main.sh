5b3468e8a5d915f09787b98afebfd13fe7ed2190
louiscklaw <louiscklaw@gmail.com>
2022-01-11 10:37:44 +0800

    update,

---
+++ puppeteer-react-multihost-vm/scripts/test/main.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -ex
+
+scripts/test/setup.sh
+
+pushd tests_host
+  yarn --dev
+popd
+
+pushd tests_host
+  yarn run int_test
+popd
+
+echo 'test done'
+
+scripts/test/clear.sh
+
