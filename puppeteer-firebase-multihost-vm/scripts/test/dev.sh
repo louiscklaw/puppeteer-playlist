93b5e86f39dea19759a1fc6980cf933f789e2c6e
louiscklaw <louiscklaw@gmail.com>
2022-01-12 09:28:01 +0800

    update,

--- puppeteer-firebase-multihost-vm/scripts/test/dev.sh
+++ puppeteer-firebase-multihost-vm/scripts/test/dev.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+pushd react_host_admin/App
+  yarn test
+popd
+
+pushd react_host_cms/App
+  yarn test
+popd
+
+pushd react_host_client/App
+  yarn test
+popd
+
+
 pushd tests_src
   yarn run int_test
 
