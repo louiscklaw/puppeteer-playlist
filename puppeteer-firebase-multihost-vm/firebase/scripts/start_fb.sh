c7794ed9882e93c9885e691d5fe15cd151355678
louiscklaw <louiscklaw@gmail.com>
2022-01-12 08:47:12 +0800

    update,

--- puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
+++ puppeteer-firebase-multihost-vm/firebase/scripts/start_fb.sh
@@ -4,32 +4,27 @@ set -ex
 
 if [[ -z "${CI}" ]]; then
   # when the build triggered by local dev
-  pushd functions
-    npm install
-  popd
-
-  nice firebase --project react-tryout-6d7dd emulators:start
-
+  # no need to install firebase tools every time
 else
   # when the build triggered by jenkins
   # curl -sL https://firebase.tools | upgrade=true bash
   firebase --version
-
-  pushd functions
-    npm install
-  popd
-
-  nice firebase --project react-tryout-6d7dd emulators:start &
-
-  # ./scripts/wait-for-it.sh -t 120 localhost:9199
-  ./scripts/wait-for-it.sh -t 120 localhost:9099
-  ./scripts/wait-for-it.sh -t 120 localhost:9000
-  ./scripts/wait-for-it.sh -t 120 localhost:8080
-  ./scripts/wait-for-it.sh -t 120 localhost:5001
-  # ./scripts/wait-for-it.sh -t 120 localhost:4000
-
-  sleep 10
-
-  echo 'firebase setup done'
-
 fi
+
+# install packages by functions, common to both local dev and CI
+pushd functions
+  npm install
+popd
+
+nice firebase --project react-tryout-6d7dd emulators:start &
+
+# ./scripts/wait-for-it.sh -t 120 localhost:9199
+./scripts/wait-for-it.sh -t 120 localhost:9099
+./scripts/wait-for-it.sh -t 120 localhost:9000
+./scripts/wait-for-it.sh -t 120 localhost:8080
+./scripts/wait-for-it.sh -t 120 localhost:5001
+# ./scripts/wait-for-it.sh -t 120 localhost:4000
+
+sleep 10
+
+echo 'firebase setup done'
