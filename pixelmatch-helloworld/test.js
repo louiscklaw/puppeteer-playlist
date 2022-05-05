73af16de412f81ff55b637dfe9e3bf0ac381e23d
louiscklaw <louiscklaw@gmail.com>
2022-05-05 17:22:49 +0800

    update pixelmatch-helloworld,

--- pixelmatch-helloworld/test.js
+++ pixelmatch-helloworld/test.js
@@ -1,32 +1,29 @@
-const fs = require( 'fs' );
-const PNG = require( 'pngjs' ).PNG;
-const pixelmatch = require( 'pixelmatch' );
+const fs = require('fs');
+const PNG = require('pngjs').PNG;
+const pixelmatch = require('pixelmatch');
 
-const example_png = PNG.sync.read( fs.readFileSync( 'example.png' ) );
-const facebook_png = PNG.sync.read( fs.readFileSync( 'facebook.png' ) );
+const example_png = PNG.sync.read(fs.readFileSync('example.png'));
+const facebook_png = PNG.sync.read(fs.readFileSync('facebook.png'));
 
-const {
+const { width, height } = example_png;
+
+const diff = new PNG({
   width,
-  height
-} = example_png;
-
-const diff = new PNG( {
+  height,
+});
+const test_same_png = new PNG({
   width,
-  height
-} );
-const test_same_png = new PNG( {
-  width,
-  height
-} );
+  height,
+});
 
-const threshold_v = .001;
+const threshold_v = 0.001;
 
-pixelmatch( example_png.data, facebook_png.data, diff.data, width, height, {
-  threshold: threshold_v
-} );
-pixelmatch( example_png.data, example_png.data, test_same_png.data, width, height, {
-  threshold: threshold_v
-} )
+pixelmatch(example_png.data, facebook_png.data, diff.data, width, height, {
+  threshold: threshold_v,
+});
+pixelmatch(example_png.data, example_png.data, test_same_png.data, width, height, {
+  threshold: threshold_v,
+});
 
 // console.log(diff.data)
 // console.log(diff.data[0]);
@@ -35,45 +32,43 @@ pixelmatch( example_png.data, example_png.data, test_same_png.data, width, heigh
 // console.log(diff.data[3]);
 // console.log(test_same_png.data)
 
-fs.writeFileSync( 'difference.png', PNG.sync.write( diff ) );
-fs.writeFileSync( 'same.png', PNG.sync.write( test_same_png ) )
+fs.writeFileSync('difference.png', PNG.sync.write(diff));
+fs.writeFileSync('same.png', PNG.sync.write(test_same_png));
 
-if ( findColorInPng( diff.data ) ) {
-  console.log( 'different png' )
+if (findColorInPng(diff.data)) {
+  console.log('different png');
 } else {
-  console.log( 'same png' )
+  console.log('same png');
 }
 
-if ( findColorInPng( test_same_png.data ) ) {
-  console.log( 'different png' )
+if (findColorInPng(test_same_png.data)) {
+  console.log('different png');
 } else {
-  console.log( 'same png' )
+  console.log('same png');
 }
 
-function getRGBArray( in_png_buffer ) {
+function getRGBArray(in_png_buffer) {
   var output = [];
   var j = -1;
-  for ( i = 0; i < in_png_buffer.length; i = i + 4 ) {
-    output[ j ] = [ in_png_buffer[ i ], in_png_buffer[ i + 1 ], in_png_buffer[ i + 2 ], in_png_buffer[ i + 3 ] ]
+  for (i = 0; i < in_png_buffer.length; i = i + 4) {
+    output[j] = [in_png_buffer[i], in_png_buffer[i + 1], in_png_buffer[i + 2], in_png_buffer[i + 3]];
     j = j + 1;
   }
   return output;
 }
 
-function checkIfColorExist( test_color1, test_color_2 ) {
-  return JSON.stringify( test_color1 ) == JSON.stringify( test_color_2 );
+function checkIfColorExist(test_color1, test_color_2) {
+  return JSON.stringify(test_color1) == JSON.stringify(test_color_2);
 }
 
-function checkIfColorExistInPic( in_RGB_array_1, in_highlight_color ) {
-  return in_RGB_array_1.filter(
-    ( x ) => {
-      return checkIfColorExist( x, in_highlight_color )
-    }
-  );
+function checkIfColorExistInPic(in_RGB_array_1, in_highlight_color) {
+  return in_RGB_array_1.filter((x) => {
+    return checkIfColorExist(x, in_highlight_color);
+  });
 }
 
-function findColorInPng( in_png_buffer ) {
-  var highlight_color = [ 255, 0, 0, 255 ];
-  var RGB_array = getRGBArray( in_png_buffer );
-  return checkIfColorExistInPic( RGB_array, highlight_color ).length > 0;
+function findColorInPng(in_png_buffer) {
+  var highlight_color = [255, 0, 0, 255];
+  var RGB_array = getRGBArray(in_png_buffer);
+  return checkIfColorExistInPic(RGB_array, highlight_color).length > 0;
 }
