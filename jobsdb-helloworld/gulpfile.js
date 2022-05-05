6cdd5e86661de6b2c2bb8f46ab9bda7d2a54bbcf
louiscklaw <louiscklaw@gmail.com>
2022-05-05 17:22:42 +0800

    update jobsdb-helloworld,

--- jobsdb-helloworld/gulpfile.js
+++ jobsdb-helloworld/gulpfile.js
@@ -4,23 +4,15 @@ const { series, parallel } = require('gulp');
 var run = require('gulp-run-command').default;
 
 const CLEAN_CMD = 'rm -rf jobsdb_qa.png';
-const BUILD_CMD = 'yarn build';
+const BUILD_CMD = 'node test.js';
 const WATCH_DIR = '.';
 
 gulp.task('clean', run(CLEAN_CMD));
 
-gulp.task(
-  'build',
-  gulp.series(
-    'clean',
-    run(BUILD_CMD, {
-      ignoreErrors: true,
-    })
-  )
-);
+gulp.task('build', gulp.series('clean', run(BUILD_CMD, { ignoreErrors: true })));
 
 gulp.task('watch', () => {
-  gulp.watch(WATCH_DIR, gulp.series('build'));
+  gulp.watch(['js/*.js', '!js/*.min.js'], gulp.series('build'));
 });
 
 gulp.task('default', gulp.series('watch'));
