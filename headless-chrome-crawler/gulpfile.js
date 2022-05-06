3713d66b7f1b94e01a94c0913db0f5991ba8983c
louiscklaw <louiscklaw@gmail.com>
2022-05-07 00:36:59 +0800

    update headless-chome-crawler,

---
+++ headless-chrome-crawler/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+
+const { series, parallel } = require('gulp');
+var run = require('gulp-run-command').default;
+
+const CLEAN_CMD = 'rm -rf jobsdb_qa.png';
+const BUILD_CMD = 'node test.js';
+const WATCH_DIR = '.';
+
+gulp.task('clean', run(CLEAN_CMD));
+
+gulp.task('build', gulp.series('clean', run(BUILD_CMD, { ignoreErrors: true })));
