be1446ed82f9ffeafdbde327300768545f0025b6
louiscklaw <louiscklaw@users.noreply.github.com>
2022-11-19 17:56:07 +0800

    update,

--- browserless/helloworld/start_docker.sh
+++ browserless/helloworld/start_docker.sh
@@ -15,5 +15,15 @@ set -ex
   # -e "DEFAULT_LAUNCH_ARGS=[\"--window-size=1920,1080\"]" \
   # -e "FUNCTION_ENABLE_INCOGNITO_MODE=true" \
 
+docker kill `docker ps -qa` || true
+sleep 1
+
+docker container prune -f
+docker system prune -f
+docker image prune -f
+docker volume prune -f
+docker network prune -f
+
+
 docker run -p 3000:3000 --restart always -d \
   --name browserless-helloworld browserless/chrome
