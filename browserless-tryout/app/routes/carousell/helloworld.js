5c6361515ab3f4e75666a61c1b353febed4e27b3
louiscklaw <louiscklaw@users.noreply.github.com>
2022-11-30 00:09:19 +0800

    update browserless-tryout,

---
+++ browserless-tryout/app/routes/carousell/helloworld.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer-core');
+const Diff = require('diff');
+
+var express = require('express');
+var router = express.Router();
+
+var config = require('../../config');
+const removeSportlight = require('../utils/removeSportlight');
+const clearAds = require('../utils/clearAds');
+const getPosts = require('../utils/getPosts');
+
+// http://localhost:8080/debug/helloworld
+
+const { BROWSER_WEBSOCKET_URL } = config;
+
+var test_json = { hello: 'world' };
+
+var STORE_PATH = `${config.STORE_PATH}/carousell`;
+
+module.exports = function (app) {
+  app.get('/carousell/helloworld', function (req, res) {
+    fs.writeFileSync(`${STORE_PATH}/helloworld.json`, JSON.stringify({ hello: 'world' }), { encoding: 'utf-8' });
+    res.send('helloworld');
+  });
+};
--- browserless-tryout/app/test.js
+++ browserless-tryout/app/test.js
@@ -4,12 +4,19 @@ var route_debug = require('./routes/debug.js');
 var route_jobsdb = require('./routes/jobsdb.js');
 var route_carousell = require('./routes/carousell.js');
 
+var carousell_helloworld = require('./routes/carousell/helloworld');
+
 const app = express();
 
 app.use(express.static('public'));
 
 app.use('/debug', route_debug);
 app.use('/jobsdb', route_jobsdb);
-app.use('/carousell', route_carousell);
+
+// app.use('/carousell/helloworld', carousell_helloworld);
+// app.use('/carousell', route_carousell);
+require('./routes/carousell/helloworld')(app);
+require('./routes/carousell/capture_carousell')(app);
+
 
 app.listen(8080);
