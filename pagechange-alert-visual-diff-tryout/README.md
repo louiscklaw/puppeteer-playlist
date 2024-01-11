4cbb07d1fe7fea25304dc3628b5c6e4f8c9225da
Louis Law <louiscklaw@users.noreply.github.com>
2024-01-11 13:20:01 +0800

    update visual diff,

---
+++ pagechange-alert-visual-diff-tryout/README.md
@@ -0,0 +1,11 @@
+# README.md
+
+
+```bash
+# install deps
+$ yarn -D
+
+# run test
+$ node ./test.js
+
+```
--- pagechange-alert-visual-diff-tryout/command_test.js
+++ pagechange-alert-visual-diff-tryout/command_test.js
@@ -1,14 +1,14 @@
-const child_process=require('child_process');
+const child_process = require('child_process');
 
 let page_name = 'page name'
-let text_to_send=`page changed ${page_name}`
-let webhook='https://hooks.slack.com/services/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'
-let username='pagechange-alert-visual-diff-tryout'
-let channel='#_debug'
+let text_to_send = `page changed ${page_name}`
+let webhook = 'https://hooks.slack.com/services/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'
+let username = 'pagechange-alert-visual-diff-tryout'
+let channel = '#_debug'
 
 // curl -vLk https://hooks.slack.com/services/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 -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}'
 
-let send_slack_msg_command=`curl https://hooks.slack.com/services/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 -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}'`;
+let send_slack_msg_command = `curl https://hooks.slack.com/services/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 -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}'`;
 
 
 child_process.execSync(send_slack_msg_command);
